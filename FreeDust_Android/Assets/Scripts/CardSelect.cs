6aaced9ef0f2116d4605fa84fe1f78a4480ad2ab
salmon <seungho6858@gmail.com>
2019-01-13 02:45:54 +0900

    카드선택 스크롤링

---
+++ FreeDust_Android/Assets/Scripts/CardSelect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSelect : MonoBehaviour {
+
+    Card[] _cards;
+
+   
+
+    private void Awake()
+    {
+        _cards = new Card[5];
+
+        GameObject prefab = Resources.Load("Prefab/Proto/MyCard") as GameObject;
+
+        Transform trParent = transform.Find("Cards/Grid");
+
+        for (int i = 0; i < _cards.Length; ++i)
+        {
+            GameObject temp = Instantiate(prefab, Vector3.zero, Quaternion.identity, trParent);
+
+            temp.layer = 20;
+
+            _cards[i] = temp.GetComponent<Card>();
+            _cards[i].Set_CardType(CARD_TYPE.END);
+
+            EventDelegate selectEvent = new EventDelegate(this, "OnClickCard");
+            selectEvent.parameters[0].value = i;
+            EventDelegate.Add(_cards[i].GetComponent<UIButton>().onClick, selectEvent);
+            
+        }
+        trParent.GetComponent<UIGrid>().Reposition();
+    }
+
+    public void OnClickCard(int index)
+    {
+        for (int i = 0; i < _cards.Length; ++i)
+        {
+            if (i == index)
+            {
+                _cards[i].OnSelected();
+            }
+            else
+            {
+                _cards[i].onDeSelected();
+            }
+        }
+    }
+
+    
+}
---
+++ FreeDust_Android/Assets/Scripts/UsefulFunction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UsefulFunction {
+
+	public static int FindNearest(float point, float[] area)
+    {
+        int result = 0;
+
+        float smallest = Mathf.Abs(point - area[0]);
+
+        for(int i=1; i<area.Length; ++i)
+        {
+            float temp = Mathf.Abs(point - area[i]);
+
+            if (temp < smallest)
+            {
+                result = i;
+                smallest = temp;
+            }
+        }
+
+        return result;
+    }
+}
